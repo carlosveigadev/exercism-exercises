44ae9b082a27a9e748072513eea7d7c1bf1fa3fc
wrakc <carlosveiga1618@gmail.com>
2020-07-11 15:27:11 -0300

    finish rna_transcription exercism

--- rna-transcription/rna_transcription.rb
+++ rna-transcription/rna_transcription.rb
@@ -1,8 +1,21 @@
 module Complement
-  def of_dna(dna)
-    dna.each do
+  def self.of_dna(dna)
+    rna = ""
+    dna.each_char do
       |n|
-      if n === 'C'
+      case n
+      when "G"
+        rna.insert(-1, "C")
+      when "C"
+        rna.insert(-1, "G")
+      when "T"
+        rna.insert(-1, "A")
+      when "A"
+        rna.insert(-1, "U")
+      else
+        return " "
+      end
     end
+    rna
   end
 end
