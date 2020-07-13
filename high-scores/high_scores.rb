0c0220d1818b0dd8d2a41625b9597443797873dc
wrakc <carlosveiga1618@gmail.com>
2020-07-13 17:32:22 -0300

    finish challange high_scores

--- high-scores/high_scores.rb
+++ high-scores/high_scores.rb
@@ -1,8 +1,26 @@
-=begin
-Write your code for the 'High Scores' exercise in this file. Make the tests in
-`high_scores_test.rb` pass.
+class HighScores
+  def initialize(data)
+    @data = data
+  end
 
-To get started with TDD, see the `README.md` file in your
-`ruby/high-scores` directory.
-=end
+  def scores
+    @data
+  end
 
+  def latest
+    @data[-1]
+  end
+
+  def personal_best
+    @data.max
+  end
+
+  def personal_top_three
+    @data.max(3)
+  end
+
+  def latest_is_personal_best?
+    @data[-1] === @data.max ? true : false
+  end
+
+end
