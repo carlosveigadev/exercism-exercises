be98fd634f3d211bdb2e8b53ef275a96d8006e00
wrakc <carlosveiga1618@gmail.com>
2020-07-15 11:51:41 -0300

    add space_age challange
    fix pangram issues

--- pangram/pangram.rb
+++ pangram/pangram.rb
@@ -1,6 +1,7 @@
 module Pangram
   def self.pangram?(sentence)
-
+    abc = 'abcdefghijklmnopqrstuvwxyz'
+    abc_array = abc.split('')
+    abc_array.all? { |c| sentence.downcase.include?(c) }
   end
 end
-
