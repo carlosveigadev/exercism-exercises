f130c1969570bc6d6db52ab721e5fab0f6e5e6a6
wrakc <carlosveiga1618@gmail.com>
2020-07-30 12:15:25 -0300

    add resolution to difference_of_square exercise

--- difference-of-squares/difference_of_squares.rb
+++ difference-of-squares/difference_of_squares.rb
@@ -1,8 +1,19 @@
-=begin
-Write your code for the 'Difference Of Squares' exercise in this file. Make the tests in
-`difference_of_squares_test.rb` pass.
+class Squares
+  def initialize(n)
+    @n = n
+  end
 
-To get started with TDD, see the `README.md` file in your
-`ruby/difference-of-squares` directory.
-=end
+  def square_of_sum
+    sum_squared = Array.new(@n) { |i| i + 1 }
+    sum_squared.sum**2
+  end
 
+  def sum_of_squares
+    squared_sum = Array.new(@n) { |i| (i + 1)**2 }
+    squared_sum.sum
+  end
+
+  def difference
+    square_of_sum - sum_of_squares
+  end
+end
