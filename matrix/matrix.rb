ccc432134ce26e916896b4a195de88889ef06906
wrakc <carlosveiga1618@gmail.com>
2020-08-03 22:52:31 -0300

    add matrix.rb improvements

--- matrix/matrix.rb
+++ matrix/matrix.rb
@@ -1,33 +1,15 @@
 class Matrix
+
+  attr_reader :rows, :columns
+
   def initialize(matrix)
     @matrix = matrix
+    @rows = create_matrix
+    @columns = @rows.transpose
   end
-
-  private
 
   def create_matrix
-    @matrix_array = []
-    @finished_matrix = []
-    @matrix_array = @matrix.scan(/\d+/).map(&:to_i)
-    @matrix_size = @matrix_array.size / (1 + @matrix.count("\n"))
-    @matrix_array.each_slice(@matrix_size) { |a| @finished_matrix << a }
-  end
-
-  public
-
-  def rows
-    create_matrix
-    @finished_matrix
-  end
-
-  def columns
-    create_matrix
-    @matrix_columns = Array.new(@matrix_size) { Array.new(0) }
-    @finished_matrix.each_with_index do |_element, i|
-      @finished_matrix[i].each_with_index do |_element, j|
-        @matrix_columns[j] << @finished_matrix[i][j]
-      end
-    end
-    @matrix_columns
+    @matrix = @matrix.each_line.map(&:split)
+    @matrix = @matrix.map { |n| n.map(&:to_i) }
   end
 end
